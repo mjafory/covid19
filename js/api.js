d7914cd5457f8addf1f31f9374fc42df660e5d10
MH Jafory <mhjafory@MHs-MacBook-Pro.local>
2020-03-23 17:02:00 -0400

    ADDED LIVE UPDATEs

--- js/api.js
+++ js/api.js
@@ -29,10 +29,10 @@ fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php", {
 
 //Fetching The Case by Country Data
 fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php", {
-    "method": "GET",
-    "headers": {
-        "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
-        "x-rapidapi-key": "bcc93bfd73msh44b00a724a0e158p14e693jsna9192b86aa01"
+   "method": "GET",
+	"headers": {
+		"x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
+		"x-rapidapi-key": "bcc93bfd73msh44b00a724a0e158p14e693jsna9192b86aa01"
     }
 })
 .then(response => response.json().then(data =>{
@@ -48,14 +48,21 @@ fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.p
         let deaths = row.insertCell(2);
         let serious_critical = row.insertCell(3);
         let recovered_per_country = row.insertCell(4);
+        let active_cases = row.insertCell(5);
+        let new_deaths = row.insertCell(6);
         country_name.innerHTML = countries_stat[i].country_name;
         cases.innerHTML = countries_stat[i].cases;
         deaths.innerHTML = countries_stat[i].deaths;
         serious_critical.innerHTML = countries_stat[i].serious_critical;
-        recovered_per_country.innerHTML = countries_stat[i].total_recovered;
+        recovered_per_country.innerHTML = countries_stat[i].total_recovered; 
+        active_cases.innerHTML = countries_stat[i].active_cases; 
+        new_deaths.innerHTML = countries_stat[i].new_deaths;
 
     }
 }))
 .catch(err => {
     console.log(err);
 });
+
+
+
