74de2ddcd0e186390406c9c72138645a2fb01c2e
Jafory-Software <jafory-software@Jafory-Softwares-MacBook-Pro.local>
2020-08-24 01:43:44 -0400

    updated

--- js/api.js
+++ js/api.js
@@ -46,16 +46,16 @@ fetch("https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.p
         let country_name = row.insertCell(0);
         let cases = row.insertCell(1);
         let deaths = row.insertCell(2);
-        let serious_critical = row.insertCell(3);
-        let recovered_per_country = row.insertCell(4);
-        let active_cases = row.insertCell(5);
-        let new_deaths = row.insertCell(6);
+//        let serious_critical = row.insertCell(3);
+        let recovered_per_country = row.insertCell(3);
+        let new_cases = row.insertCell(4);
+        let new_deaths = row.insertCell(5);
         country_name.innerHTML = countries_stat[i].country_name;
         cases.innerHTML = countries_stat[i].cases;
         deaths.innerHTML = countries_stat[i].deaths;
-        serious_critical.innerHTML = countries_stat[i].serious_critical;
+//        serious_critical.innerHTML = countries_stat[i].serious_critical;
         recovered_per_country.innerHTML = countries_stat[i].total_recovered; 
-        active_cases.innerHTML = countries_stat[i].active_cases; 
+        new_cases.innerHTML = countries_stat[i].new_cases; 
         new_deaths.innerHTML = countries_stat[i].new_deaths;
 
     }
